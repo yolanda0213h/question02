c1ab10422aff8487b4985a2ab0b8ddde73311679
yolanda0213 <yolanda0213h@gmail.com>
2019-02-12 13:45:54 +0800

    done

--- question/ViewController.swift
+++ question/ViewController.swift
@@ -11,13 +11,15 @@ var star = ""
 var score = 0
 class ViewController: UIViewController {
     var index = 0
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var noanswerButton: UIButton!
+    @IBOutlet weak var textfieldContent: UITextField!
     @IBOutlet var pickings: [UIButton]!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
-    @IBOutlet weak var showButton: UIButton!
     @IBOutlet weak var showView: UIView!
-    var questions = [question(descriptionText: "茶葉根據發酵程度可分為不發酵茶、部分發酵茶與全發酵茶，那麼不需要發酵製作工藝的茶葉是哪種？", answerText: "綠茶", chooseText:["紅茶","綠茶","黑茶"]),question(descriptionText: "咖啡樹適合成長在熱帶和亞熱帶氣候的地區，也就是位於南北迴歸線之間，以赤道為中心，約於北緯25度到南緯30度之間，並形成一個環狀地帶稱之為", answerText: "咖啡腰帶", chooseText:["咖啡赤道","咖啡歸線","咖啡腰帶"]),question(descriptionText: "那一個冷藏溫度比較適合新鮮果汁", answerText: "4℃", chooseText:["4℃","12℃","20℃"]),question(descriptionText: "使用個人式茶具沖泡茶葉時，3g的茶葉量，最好使用多少的水量為佳？", answerText: "150㏄", chooseText: ["100cc","120㏄","150㏄"]),question(descriptionText: "人的舌頭味覺分佈是", answerText: "舌尖甜兩邊酸舌根苦", chooseText: ["舌尖甜兩邊苦舌根酸","舌尖甜兩邊酸舌根苦","舌尖酸兩邊甜舌根苦"]),question(descriptionText: "下列那一種茶的維生素Ｃ含量最多？", answerText: "龍井綠茶", chooseText: ["茉莉花茶","菊花茶","龍井綠茶"]),question(descriptionText: "台灣氣候適宜種植茶樹，一年最多可採收幾次", answerText: "六次", chooseText: ["兩次","四次","六次"]),question(descriptionText: "依據CNS規定，製作冰淇淋至少應含有多少比例之鮮奶油乳脂肪", answerText: "8%", chooseText: ["4%","6%","8%"]),question(descriptionText: "鮮奶的飲料會發生凝結現象，是因為加入了含有什麼成份的食品", answerText: "酸性", chooseText: ["糖份","酸性","脂肪"]),question(descriptionText: "下列何物是略帶鹼性(不甜)的碳酸飲料", answerText: "蘇打水(Soda Water)", chooseText: ["薑汁汽水(Ginger Ale)","蘇打水(Soda Water)","萊姆汁(Lime Juice)"])]
+var questions = [question(descriptionText: "新的一年是己亥年，又稱哪一個“生肖年”？", answerText: "豬年", chooseText:["雞年","豬年","魚年"]),question(descriptionText: "新年吉祥花卉「金盞銀臺」，指的是什麼花？", answerText: "水仙花", chooseText:["銀柳","水仙花","牡丹花"]),question(descriptionText: "傳統初五開工是因為迎接哪個神的生日？", answerText: "財神", chooseText:["灶神","宅神","財神"]),question(descriptionText: "過年要吃長年菜，在台灣中北部吃的是芥菜，在南部吃的是什麼菜？", answerText: "菠菜", chooseText: ["菠菜","韭菜","香菜"]),question(descriptionText: "初三睡到飽，這天又稱為”什麼日“？", answerText: "赤狗日", chooseText: ["赤狗日","白虎日","朱雀日"]),question(descriptionText: "玉皇大帝生日是初幾？", answerText: "初九", chooseText: ["初六","初九","初八"]),question(descriptionText: "春節的最後一天是上元節，這天是正月幾號？", answerText: "十五", chooseText: ["十五","十二","三十"]),question(descriptionText: "和尚撐傘（猜一成語）", answerText: "無法無天", chooseText: ["法海無邊","一手遮天","無法無天"]),question(descriptionText: "一顆心值多少錢？（跟成語有關）", answerText: "一億", chooseText: ["一億","八億","一元"]),question(descriptionText: "正月沒有初一（猜一字）", answerText: "肯", chooseText: ["口","肯","丹"])]
     func showChoose() {
         pickings.shuffle()
         for i in 0 ..< pickings.count {
@@ -29,7 +31,7 @@ class ViewController: UIViewController {
         self.pickings[0].alpha = 0
         self.pickings[1].alpha = 0
         self.pickings[2].alpha = 0
-        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1, delay: 1, animations: {
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1, delay: 0, animations: {
             self.pickings[0].alpha = 1
             self.pickings[1].alpha = 1
             self.pickings[2].alpha = 1
@@ -42,68 +44,105 @@ class ViewController: UIViewController {
         questions.shuffle()
         questionLabel.text = "第1題\n\n" + questions[0].description
         questionLabel.isHidden = false
-        showChoose()
+        pickings[0].alpha = 0
+        pickings[1].alpha = 0
+        pickings[2].alpha = 0
     }
     override func viewDidDisappear(_ animated: Bool) {
         questionLabel.text = ""
         answerLabel.text = ""
-        showButton.alpha = 0
         for i in 0 ..< pickings.count {
             pickings[i].alpha = 0
         }
         
     }
+    @IBAction func showchoose(_ sender: UIButton) {
+        sendButton.alpha = 1
+        if sender.currentTitle == "提示1次" {
+            showChoose()
+            sender.backgroundColor = UIColor(displayP3Red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+            sender.setTitleColor(UIColor.white, for: UIControl.State.normal)
+            sender.setTitle("提示用盡", for: UIControl.State.normal)
+        }else if sender.currentTitle == "放棄"{
+            answerLabel.isHidden = false
+            answerLabel.text = "正確答案是\n\n" + questions[index].answer
+            answerLabel.textColor = UIColor.yellow
+            for i in 0 ..< pickings.count {
+                pickings[i].isEnabled = false
+            }
+            nextButton.alpha = 1
+        }
+        
+    }
     @IBAction func nextFunc(_ sender: UIButton) {
+        
         index = index + 1
         if index == 3 {
             index = 0
+            sendButton.alpha = 0
             sender.setTitle("挑戰第二題", for: UIControl.State.normal)
             questions.shuffle()
             score = 0
             star = ""
+            sendButton.isEnabled = true
+            noanswerButton.setTitle("提示1次", for: UIControl.State.normal)
+            noanswerButton.backgroundColor = UIColor.yellow
+            noanswerButton.setTitleColor(UIColor.blue, for: UIControl.State.normal)
         } else if index == 1 {
             sender.setTitle("挑戰最後一題", for: UIControl.State.normal)
+            if noanswerButton.currentTitle == "提示用盡" {
+                noanswerButton.setTitle("放棄", for: UIControl.State.normal)
+            }
         } else if index == 2 {
             sender.setTitle("重新挑戰", for: UIControl.State.normal)
-            
+            if noanswerButton.currentTitle == "提示用盡" {
+                noanswerButton.setTitle("放棄", for: UIControl.State.normal)
+            }
         }
         questionLabel.text = "第" + String(index+1) + "題\n\n" + questions[index].description
-        showChoose()
         answerLabel.isHidden = true
         answerLabel.text = ""
+        pickings[0].alpha = 0
+        pickings[1].alpha = 0
+        pickings[2].alpha = 0
+        textfieldContent.text = ""
         sender.alpha = 0
-        showButton.alpha = 0
+        sendButton.alpha = 0
     }
-    
-    
-    @IBAction func picking3(_ sender: UIButton) {
-        answerLabel.isHidden = false
-        if sender.currentTitle == questions[index].answer{
-            answerLabel.text = "答對了！"
-            sender.backgroundColor = UIColor(displayP3Red: 0, green: 0.7, blue: 0, alpha: 0.9)
-            score = score + 1
-            star = star + " ⭐️ "
-            if index == 2 {
-                showButton.alpha = 1
-                showButton.setTitle("我要摘星", for: UIControl.State.normal)
-            }else{
-                
-            }
+    @IBAction func didendonexit(_ sender: UITextField) {
+        if sender.text == "" {
+            sendButton.alpha = 0
         }else{
-            answerLabel.text = "答錯了！正確答案是\n\n" + questions[index].answer
-            sender.backgroundColor = UIColor(displayP3Red: 0.7, green: 0, blue: 0, alpha: 0.9)
-            if index == 2 {
-                showButton.alpha = 1
-                showButton.setTitle("我要摘星", for: UIControl.State.normal)
-            }else{
-                
+            sendButton.alpha = 1
+        }
+    }
+    @IBAction func showanswer(_ sender: UIButton) {
+        
+        pickings[0].alpha = 0
+        pickings[1].alpha = 0
+        pickings[2].alpha = 0
+        if textfieldContent.text == "" {
+            answerLabel.isHidden = false
+            answerLabel.text = "請輸入答案"
+            answerLabel.textColor = UIColor.yellow
+         } else {
+            sender.isEnabled = true
+            answerLabel.isHidden = false
+            if textfieldContent.text == questions[index].answer {
+                answerLabel.text = "答對了！"
+                answerLabel.textColor = UIColor.green
+            }else {
+                answerLabel.text = "答錯了！\n\n正確答案是\n\n" + questions[index].answer
+                answerLabel.textColor = UIColor.yellow
             }
+            for i in 0 ..< pickings.count {
+                pickings[i].isEnabled = false
+            }
+            nextButton.alpha = 1
         }
-        sender.setTitleColor(UIColor(displayP3Red: 1, green: 1, blue: 1, alpha: 1), for: UIControl.State.normal)
-        for i in 0 ..< pickings.count {
-            pickings[i].isEnabled = false
-        }
-        nextButton.alpha = 1
+    }
+    @IBAction func picking3(_ sender: UIButton) {
+        textfieldContent.text = sender.currentTitle
     }
 }
 
